8d4f1b310e04705bee680428c9ee26bc95df7b52
Chris Padwick <cgpadwick@gmail.com>
2025-05-10 13:50:07 -0700

    run the camera in mjpg mode

--- src/ros_vision/src/usb_camera/src/usb_camera_node.cpp
+++ src/ros_vision/src/usb_camera/src/usb_camera_node.cpp
@@ -9,11 +9,17 @@ public:
         : Node("camera_publisher"), cap_(0) {
 
         RCLCPP_INFO(this->get_logger(), "Opening camera on idx: '%d'", 0);
-        cap_.open(0);
+        cap_.open(0, cv::CAP_V4L2);
         if (!cap_.isOpened()) {
             RCLCPP_ERROR(this->get_logger(), "Could not open camera");
             throw std::runtime_error("Camera not opened");
         }
+
+        int fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+        cap_.set(cv::CAP_PROP_FOURCC, fourcc);
+        cap_.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+        cap_.set(cv::CAP_PROP_FRAME_HEIGHT, 800);
+        cap_.set(cv::CAP_PROP_CONVERT_RGB, true);
 
         RCLCPP_INFO(this->get_logger(), "Width: '%d'", static_cast<int>(cap_.get(cv::CAP_PROP_FRAME_WIDTH)));
         RCLCPP_INFO(this->get_logger(), "Height: '%d'", static_cast<int>(cap_.get(cv::CAP_PROP_FRAME_HEIGHT)));
