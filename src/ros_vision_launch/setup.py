19448b758dfb6e1071030214a8200f06738e7321
Chris Padwick <cgpadwick@gmail.com>
2025-06-22 16:03:05 -0700

    forgot the setup file

---
+++ src/ros_vision_launch/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros_vision_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/launch_vision.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cpadwick',
+    maintainer_email='cgpadwick@gmail.com',
+    description='ROS2 Vision Launch Utilities',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
