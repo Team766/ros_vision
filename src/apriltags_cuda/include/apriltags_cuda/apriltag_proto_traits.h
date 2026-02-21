e7b1ca329e20142627cc26773dd18eca6d8bd78f
Debajit Ghosh <dg@dejabotz.com>
2026-02-20 20:08:16 -0800

    attempt to fix the protobuf serialization issue

--- src/apriltags_cuda/include/apriltags_cuda/apriltag_proto_traits.h
+++ src/apriltags_cuda/include/apriltags_cuda/apriltag_proto_traits.h
@@ -12,51 +12,55 @@ namespace wpi {
 
 template <>
 struct Protobuf<com::team766::vision::ApriltagProto> {
-  using Message = com::team766::vision::ApriltagProto;
+  using Message = google::protobuf::Message;
   static std::string_view GetTypeName() {
     return "com.team766.vision.ApriltagProto";
   }
   static google::protobuf::Descriptor const* GetDescriptor() {
     return com::team766::vision::ApriltagProto::descriptor();
   }
-  static Message New(google::protobuf::Arena* arena) {
-    if (arena) {
-      return *google::protobuf::Arena::CreateMessage<com::team766::vision::ApriltagProto>(arena);
+  static Message* New(google::protobuf::Arena* arena) {
+    return google::protobuf::Arena::CreateMessage<com::team766::vision::ApriltagProto>(arena);
+  }
+  static void Pack(Message* msg,
+                   const com::team766::vision::ApriltagProto& value) {
+    if (auto* proto = static_cast<com::team766::vision::ApriltagProto*>(msg)) {
+      *proto = value;
     }
-    return com::team766::vision::ApriltagProto{};
   }
-  static void Pack(std::span<uint8_t> out,
-                   const com::team766::vision::ApriltagProto& msg) {
-    msg.SerializeToArray(out.data(), out.size());
-  }
-  static bool Unpack(com::team766::vision::ApriltagProto* out,
-                     std::span<const uint8_t> in) {
-    return out->ParseFromArray(in.data(), in.size());
+  static com::team766::vision::ApriltagProto Unpack(const Message& msg) {
+    if (auto const* proto =
+            static_cast<com::team766::vision::ApriltagProto const*>(&msg)) {
+      return *proto;
+    }
+    return {};
   }
 };
 
 template <>
 struct Protobuf<com::team766::vision::ApriltagListProto> {
-  using Message = com::team766::vision::ApriltagListProto;
+  using Message = google::protobuf::Message;
   static std::string_view GetTypeName() {
     return "com.team766.vision.ApriltagListProto";
   }
   static google::protobuf::Descriptor const* GetDescriptor() {
     return com::team766::vision::ApriltagListProto::descriptor();
   }
-  static Message New(google::protobuf::Arena* arena) {
-    if (arena) {
-      return *google::protobuf::Arena::CreateMessage<com::team766::vision::ApriltagListProto>(arena);
+  static Message* New(google::protobuf::Arena* arena) {
+    return google::protobuf::Arena::CreateMessage<com::team766::vision::ApriltagListProto>(arena);
+  }
+  static void Pack(Message* msg,
+                   const com::team766::vision::ApriltagListProto& value) {
+    if (auto* proto = static_cast<com::team766::vision::ApriltagListProto*>(msg)) {
+      *proto = value;
     }
-    return com::team766::vision::ApriltagListProto{};
   }
-  static void Pack(std::span<uint8_t> out,
-                   const com::team766::vision::ApriltagListProto& msg) {
-    msg.SerializeToArray(out.data(), out.size());
-  }
-  static bool Unpack(com::team766::vision::ApriltagListProto* out,
-                     std::span<const uint8_t> in) {
-    return out->ParseFromArray(in.data(), in.size());
+  static com::team766::vision::ApriltagListProto Unpack(const Message& msg) {
+    if (auto const* proto =
+            static_cast<com::team766::vision::ApriltagListProto const*>(&msg)) {
+      return *proto;
+    }
+    return {};
   }
 };
 
